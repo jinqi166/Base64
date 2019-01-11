7d2a3bc570532eccc0cf4b8ae779ad38dcf3374b
Günther Foidl <gue@korporal.at>
2019-01-11 10:02:42 +0100

    Written AssertHelper with raw pointers instead of GC-tracked refs

--- source/gfoidl.Base64/Internal/AssertHelper.cs
+++ source/gfoidl.Base64/Internal/AssertHelper.cs
@@ -7,34 +7,38 @@ namespace gfoidl.Base64.Internal
     internal static class AssertHelper
     {
         [Conditional("DEBUG")]
-        public static void AssertRead<TVector, T>(this ref T src, ref T srcStart, int srcLength) where T : unmanaged
+        public static unsafe void AssertRead<TVector, T>(this ref T src, ref T srcStart, int srcLength) where T : unmanaged
         {
-            int vectorElements = GetCount<TVector, T>();
-            ref T readEnd      = ref Unsafe.Add(ref src, vectorElements);
-            ref T srcEnd       = ref Unsafe.Add(ref srcStart, srcLength + 1);
-
-            bool isSafe = Unsafe.IsAddressLessThan(ref readEnd, ref srcEnd);
-
-            if (!isSafe)
+            fixed (T* pSrc      = &src)
+            fixed (T* pSrcStart = &srcStart)
             {
-                int srcIndex = Unsafe.ByteOffset(ref srcStart, ref src).ToInt32();
-                throw new InvalidOperationException($"Read for {typeof(TVector)} is not within safe bounds. srcIndex: {srcIndex}, srcLength: {srcLength}");
+                int vectorElements = GetCount<TVector, T>();
+                T* readEnd         = pSrc + vectorElements;
+                T* srcEnd          = pSrcStart + srcLength;
+
+                if (readEnd > srcEnd)
+                {
+                    int srcIndex = (int)(pSrc - pSrcStart);
+                    throw new InvalidOperationException($"Read for {typeof(TVector)} is not within safe bounds. srcIndex: {srcIndex}, srcLength: {srcLength}");
+                }
             }
         }
         //---------------------------------------------------------------------
         [Conditional("DEBUG")]
-        public static void AssertWrite<TVector, T>(this ref T dest, ref T destStart, int destLength) where T : unmanaged
+        public static unsafe void AssertWrite<TVector, T>(this ref T dest, ref T destStart, int destLength) where T : unmanaged
         {
-            int vectorElements = GetCount<TVector, T>();
-            ref T writeEnd     = ref Unsafe.Add(ref dest, vectorElements);
-            ref T destEnd      = ref Unsafe.Add(ref destStart, destLength + 1);
-
-            bool isSafe = Unsafe.IsAddressLessThan(ref writeEnd, ref destEnd);
-
-            if (!isSafe)
+            fixed (T* pDest      = &dest)
+            fixed (T* pDestStart = &destStart)
             {
-                int destIndex = Unsafe.ByteOffset(ref destStart, ref dest).ToInt32();
-                throw new InvalidOperationException($"Write for {typeof(TVector)} is not within safe bounds. destIndex: {destIndex}, destLength: {destLength}");
+                int vectorElements = GetCount<TVector, T>();
+                T* writeEnd        = pDest + vectorElements;
+                T* destEnd         = pDestStart + destLength;
+
+                if (writeEnd > destEnd)
+                {
+                    int destIndex = (int)(pDest - pDestStart);
+                    throw new InvalidOperationException($"Write for {typeof(TVector)} is not within safe bounds. destIndex: {destIndex}, destLength: {destLength}");
+                }
             }
         }
         //---------------------------------------------------------------------
